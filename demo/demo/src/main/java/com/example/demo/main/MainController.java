b1e8c46e109e5609a463ecc3962e773a6bfb92b9
Eungyeol41 <iniziato@naver.com>
2023-12-22 15:20:09 +0900

    폴더 추가 및 경로 변경

--- demo/demo/src/main/java/com/example/demo/main/MainController.java
+++ demo/demo/src/main/java/com/example/demo/main/MainController.java
@@ -32,6 +32,6 @@ public class MainController {
         List<BoardDTO> boardList = boardService.selectList();
         model.addAttribute("boardList", boardList);
 
-        return "list";
+        return "board/list";
     }
 }
